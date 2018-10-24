369dc7766a3917d83199f791dca026bb8b64cf6b
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-24 07:56:15 +0530

    DI with spring

--- di-demo/src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ di-demo/src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -2,10 +2,13 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
-
+    //@Autowired not required this id done automatically, but informational
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- di-demo/src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
+++ di-demo/src/main/java/guru/springframework/didemo/controllers/SetterInjectedController.java
@@ -2,14 +2,17 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import guru.springframework.didemo.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
     }
-
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
