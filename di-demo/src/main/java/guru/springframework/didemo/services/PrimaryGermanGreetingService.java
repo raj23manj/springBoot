ad68d57f6905dc2995430fd61190215861b210aa
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-24 08:41:13 +0530

    Using default Profile with Primary

--- di-demo/src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ di-demo/src/main/java/guru/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary // making it default if more beans of same impl
-@Profile({"en", "default"})
-// making one profile as default, when more @Primary are there
-public class PrimaryGreetingService implements GreetingService {
+@Primary
+@Profile("de")
+// making one profile as default
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Primärer Grußdienst";
     }
 }
