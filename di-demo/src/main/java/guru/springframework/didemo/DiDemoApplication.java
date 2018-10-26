f9404e468a3192b22cca37f5256a05ca4848c248
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-26 21:52:36 +0530

    loading attributes from files in class path, using @PropertySource

--- di-demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ di-demo/src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -4,6 +4,7 @@ import guru.springframework.didemo.controllers.ConstructorInjectedController;
 import guru.springframework.didemo.controllers.MyController;
 import guru.springframework.didemo.controllers.PropertyInjectedController;
 import guru.springframework.didemo.controllers.SetterInjectedController;
+import guru.springframework.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -19,14 +20,18 @@ public class DiDemoApplication {
 
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
-        MyController controller = (MyController) ctx.getBean("myController");
+        //MyController controller = (MyController) ctx.getBean("myController");
 
         //controller.hello();
-        System.out.println("Start!!!");
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
-        System.out.println("End!!!");
+//        System.out.println("Start!!!");
+//        System.out.println(controller.hello());
+//        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+//        System.out.println("End!!!");
+
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
     }
 }
---
+++ di-demo/src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -0,0 +1,41 @@
+package guru.springframework.didemo.config;
+
+import guru.springframework.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+// bring in the file
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    // this will get into the spring context and get it as externalised property
+    @Value("${guru.username}")
+    String user;
+
+    @Value("${guru.password}")
+    String password;
+
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+
+    @Bean
+    // this will match/wire up the properties from the properties file to attributes
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
