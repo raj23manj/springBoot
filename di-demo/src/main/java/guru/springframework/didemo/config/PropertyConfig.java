8790a9fe6d2e964b56b904a3e57e09a4abe8f594
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-26 22:08:16 +0530

    Spring Environment variables

--- di-demo/src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ di-demo/src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package guru.springframework.didemo.config;
 
 import guru.springframework.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 // bring in the file
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     // this will get into the spring context and get it as externalised property
     @Value("${guru.username}")
@@ -25,7 +30,11 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+//        fakeDataSource.setUser(user); see video 95 section 6 for env
+/*
+* next to hammer > edit configurations > Environment > Environment variables set it there
+* */
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
