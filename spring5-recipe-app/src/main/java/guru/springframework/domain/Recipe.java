3c565009f3619c87e34e016bf87753d30ba6a7e9
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-28 08:35:08 +0530

    Added Enums

--- spring5-recipe-app/src/main/java/guru/springframework/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springframework/domain/Recipe.java
@@ -20,8 +20,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -31,6 +29,13 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    /*
+     section 8,  #143
+     ORDINAL gives number for each value[1,2,3]. when added to DB, later new value added to enum in between, id will change
+    * */
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     public Long getId() {
         return id;
@@ -118,5 +123,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
