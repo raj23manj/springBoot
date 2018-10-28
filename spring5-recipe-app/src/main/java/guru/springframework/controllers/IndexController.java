c698df1e268c45d3d9f7637f3ce9ba7e999b4586
rajesh23-drg <rmanjunath@teamdrg.com>
2018-10-28 14:00:29 +0530

    Assignment display recipes, different bootstrap and new iterator

--- spring5-recipe-app/src/main/java/guru/springframework/controllers/IndexController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/controllers/IndexController.java
@@ -4,8 +4,10 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import guru.springframework.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -13,23 +15,38 @@ import java.util.Optional;
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+//    private CategoryRepository categoryRepository;
+//    private UnitOfMeasureRepository unitOfMeasureRepository;
+//
+//    @Autowired // not required
+//    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+//        this.categoryRepository = categoryRepository;
+//        this.unitOfMeasureRepository = unitOfMeasureRepository;
+//    }
+//
+//
+//    @RequestMapping({"","/","/index"})
+//    public String getIndexPage() {
+//        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+//        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+//
+//        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+//        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+//
+//        return "index";
+//    }
 
-    @Autowired // not required
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
 
-    @RequestMapping({"","/","/index"})
-    public String getIndexPage() {
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM ID is: " + unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
