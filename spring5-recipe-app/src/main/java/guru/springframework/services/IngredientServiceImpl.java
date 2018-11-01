f72403eca6d26fd33ce105e35e5c7924ee111aa5
rajesh23-drg <rmanjunath@teamdrg.com>
2018-11-01 08:16:39 +0530

    delete Ingredients

--- spring5-recipe-app/src/main/java/guru/springframework/services/IngredientServiceImpl.java
+++ spring5-recipe-app/src/main/java/guru/springframework/services/IngredientServiceImpl.java
@@ -112,4 +112,30 @@ public class IngredientServiceImpl implements IngredientService {
         }
 
     }
+
+    @Override
+    public void deleteById(Long recipeId, Long idToDelete) {
+        log.debug("Deleting ingredient: " + recipeId + ":" + idToDelete);
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+        if(recipeOptional.isPresent()){
+            Recipe recipe = recipeOptional.get();
+            log.debug("found recipe");
+            Optional<Ingredient> ingredientOptional = recipe
+                    .getIngredients()
+                    .stream()
+                    .filter(ingredient -> ingredient.getId().equals(idToDelete))
+                    .findFirst();
+            if(ingredientOptional.isPresent()){
+                log.debug("found Ingredient");
+                Ingredient ingredientToDelete = ingredientOptional.get();
+                // setting null here causes hiberbnate to delete
+                ingredientToDelete.setRecipe(null);
+                recipe.getIngredients().remove(ingredientOptional.get());
+                recipeRepository.save(recipe);
+            }
+        } else {
+            log.debug("Recipe Id Not found. Id:" + recipeId);
+        }
+    }
+
 }
