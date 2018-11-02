7080a33f871e0f52aba649f074e90df57eaccf6a
rajesh23-drg <rmanjunath@teamdrg.com>
2018-11-02 12:27:01 +0530

    Refactoring for Controller-advice

--- spring5-recipe-app/src/test/java/guru/springframework/controllers/ImageControllerTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/controllers/ImageControllerTest.java
@@ -35,7 +35,9 @@ public class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                  .setControllerAdvice(new ControllerExceptionHandler())
+                  .build();
     }
 
     @Test
@@ -87,5 +89,12 @@ public class ImageControllerTest {
                 .andReturn().getResponse();
         byte[] reponseBytes = response.getContentAsByteArray();
         assertEquals(s.getBytes().length, reponseBytes.length);
+    }
+
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 }
