951543eb1e26494c44ba943469c2d17c19719caf
rajesh23-drg <rmanjunath@teamdrg.com>
2018-11-03 11:44:35 +0530

    refactoring db init, moving to default and fixed failing test case

--- spring5-recipe-app/src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -78,7 +78,7 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED) // it says it is a form that posts
                 .param("id", "")
                 .param("description", "some string")
-                .param("directions", "")
+                .param("directions", "aaa")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
